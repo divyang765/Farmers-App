17bbac10c14b0125eccc41633dff300ffff97cae
divyangbhamat <dbhmat99@gmail.com>
2024-01-26 22:18:25 +0530

    Splash Screen Implemented

---
+++ app/src/main/java/com/example/farmersapp/SplashScreen/MainViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.farmersapp.SplashScreen
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+
+    private val _isReady = MutableStateFlow(false)
+    val isReady = _isReady.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+
+            delay(3000)
+            _isReady.value = true
+
+        }
+    }
+}
